2923da5104e962f0b681f9318964ee78a23912d9
*Vasisualey* <basilnekrasov@mail.ru>
2023-02-24 20:06:46 +0300

    решил 47 задачу

---
+++ Domashka/Task47/Program.cs
@@ -0,0 +1,47 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный
+//  случайными вещественными числами.
+
+// m = 3, n = 4.
+
+Console.Clear();
+int m = Input ("Введите количество строк: ");
+int n = Input ("Введите количество столбцов: ");
+int min = Input ("Введите минимальное значение массива: ");
+int max = Input ("Введите максимальное значение массива: ");
+
+double [,] array = ArrayDouble (m, n, min, max);
+PrintArray(array);
+
+int Input (string message)
+{
+    Console.Write (message);
+    return Convert.ToInt32 (Console.ReadLine ());
+}
+
+double [,] ArrayDouble (int line, int column, int min, int max)
+{
+    double [,] numArr2D = new double [line, column];
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            numArr2D [i, j] = new Random ().Next(min, max) + new Random ().NextDouble (); 
+            numArr2D [i, j] = Math.Round (numArr2D [i, j], 2);// это округление до 2 знаков после запятой, запомнить!
+    }
+    }
+    return numArr2D;
+}
+
+
+void PrintArray(double [,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength (0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength (1); j++)
+        {
+            Console.Write (array2D [i, j] + " ");
+        }
+        Console.WriteLine ();
+    }
+}
+
--- Task51/Program.cs
+++ Task51/Program.cs
@@ -1,13 +1,14 @@
-﻿// Задача 46: Задайте двумерный массив размером m:n, 
-// заполненный случайными целыми числами.
+﻿// Задача 51. Задайте двумерный массив. Найдите сумму элементов,
+// находящихся на главной диагонали(с индексами 0.0, 1.1 и тд)
+
 Console.Write("Введите количество строк массива: ");
 int rows = int.Parse(Console.ReadLine()!);
 Console.Write("Введите количество столбцов массива: ");
 int columns = int.Parse(Console.ReadLine()!);
 
-
 int[,] array = GetArray(rows, columns, 0,10);
 Printarray(array);
+// Console.WriteLine(Getsum(array));
 
 int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
@@ -24,12 +25,34 @@ int[,] GetArray(int m, int n, int minValue, int maxValue)
 }
 void Printarray(int[,] inarray)
 {
+    int sum = 0;
     for (int i = 0; i < inarray.GetLength(0); i++)
     {
         for (int j=0; j<inarray.GetLength(1); j++)
         {
             Console.Write($"{inarray[i,j]}  ");
+            if(i == j)
+            {
+               sum += inarray[i,j];;
+            }
         }
         Console.WriteLine();
     }
+    Console.WriteLine(sum);
 }
+
+// int Getsum(int[,] inarray)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < inarray.GetLength(0); i++)
+//     {
+//         for (int j=0; j<inarray.GetLength(1); j++)
+//         {
+//             if(i == j)
+//             {
+//                sum += inarray[i,j];;
+//             }
+//         }
+//     }
+// return sum;
+// }
